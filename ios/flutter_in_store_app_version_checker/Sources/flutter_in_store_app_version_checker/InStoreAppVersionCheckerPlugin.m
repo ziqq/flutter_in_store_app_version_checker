047e03620732c09e07f909efd4e600c4f2c2f2b1
AntØn UstinØff <a.a.ustinoff@gmail.com>
2026-02-09 15:12:55 +0400

    fix(2.0.0): `InStoreAppVersionCheckerPlugin` has different definitions in different modules

--- ios/flutter_in_store_app_version_checker/Sources/flutter_in_store_app_version_checker/InStoreAppVersionCheckerPlugin.m
+++ ios/flutter_in_store_app_version_checker/Sources/flutter_in_store_app_version_checker/InStoreAppVersionCheckerPlugin.m
@@ -1,1 +1,8 @@
+#import "InStoreAppVersionCheckerPlugin.h"
+#if __has_include(<flutter_in_store_app_version_checker/flutter_in_store_app_version_checker-Swift.h>)
+#import <flutter_in_store_app_version_checker/flutter_in_store_app_version_checker-Swift.h>
+#else
+#import "flutter_in_store_app_version_checker-Swift.h"
+#endif
+
 // Intentionally empty: Swift provides `InStoreAppVersionCheckerPlugin`.
