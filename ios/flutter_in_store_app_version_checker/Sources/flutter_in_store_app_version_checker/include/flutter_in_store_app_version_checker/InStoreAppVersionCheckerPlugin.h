e4a134487bd9846423085c573bec9e57838a7149
AntØn UstinØff <a.a.ustinoff@gmail.com>
2026-02-10 15:01:37 +0400

    fix(2.0.0-pre.20): ios implementation

--- ios/flutter_in_store_app_version_checker/Sources/flutter_in_store_app_version_checker/include/flutter_in_store_app_version_checker/InStoreAppVersionCheckerPlugin.h
+++
@@ -1,8 +0,0 @@
-#import <Flutter/Flutter.h>
-
-// Re-export the Swift plugin class for Objective-C registrants.
-#if __has_include(<flutter_in_store_app_version_checker/flutter_in_store_app_version_checker-Swift.h>)
-#import <flutter_in_store_app_version_checker/flutter_in_store_app_version_checker-Swift.h>
-#else
-#import "flutter_in_store_app_version_checker-Swift.h"
-#endif
